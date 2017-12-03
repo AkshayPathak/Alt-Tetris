648afeaab529080dea4962e03d51a993c6ab06d2
Brendan Power <brpower@edu.uwaterloo.ca>
2017-12-02 22:01:02 -0500

    Updated Score.h, Score.cc

--- Score.cc
+++ Score.cc
@@ -1,29 +1,26 @@
 #include "Score.h"
 
 struct Score::ScoreImpl {
-    std::shared_ptr<Level> level;
     int hiScore;
     int currentScore;
 
-    explicit ScoreImpl(std::shared_ptr<Level>): level{level}, hiScore{0}, currentScore{0} {}
+    ScoreImpl(): hiScore{0}, currentScore{0} {}
 };
 
-//Score::Score(std::shared_ptr<Level> level): scoreImpl{std::make_unique<ScoreImpl>} {}
+Score::Score(): scoreImpl{std::make_unique<ScoreImpl>()} {}
+
+Score::~Score() {};
 
 int Score::getHiScore() {
     return scoreImpl->hiScore;
 }
 
-int Score::getCurrentScore() {
+int Score::getScore() {
     return scoreImpl->currentScore;
 }
 
-int square(int x) {
-    return x*x;
-}
 
-void Score::updateScore() {
-    int addedScore = square(scoreImpl->level->getLevel() + 1);
-    scoreImpl->currentScore += addedScore;
+void Score::updateScore(int increment) {
+    scoreImpl->currentScore += increment;
     if(scoreImpl->currentScore > scoreImpl->hiScore) scoreImpl->hiScore = scoreImpl->currentScore;
 }
