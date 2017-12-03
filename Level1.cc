ab99be7cb44e2aef0802e124b206ae4201e01e97
Brendan Power <brpower@edu.uwaterloo.ca>
2017-12-02 22:56:58 -0500

    Added probabilities for make_block in Level1, ... , Level4

--- Level1.cc
+++ Level1.cc
@@ -1,10 +1,34 @@
 #include "Level1.h"
 
+
 shared_ptr<Block> Level1::makeBlock() {
+    shared_ptr<Block> block;
 
+    int num = std::rand() % 12;
+
+    if(!num) {
+        block = make_shared<Block>(makeSBlock());
+    } else if(num == 1) {
+        block = make_shared<Block>(makeZBlock());
+    } else if(num == 2 || num == 3) {
+        block = make_shared<Block>(makeIBlock());
+    } else if(num == 4 || num == 5) {
+        block = make_shared<Block>(makeJBlock());
+    } else if(num == 6 || num == 7) {
+        block = make_shared<Block>(makeLBlock());
+    } else if(num == 8 || num == 9) {
+        block = make_shared<Block>(makeOBlock());
+    } else if(num == 10 || num == 11) {
+        block = make_shared<Block>(makeTBlock());
+    }
+
+    return block;
 }
-
 
 int Level1::getLevel() {
     return 1;
 }
+
+Level1::Level1() = default;
+
+Level1::~Level1() = default;
--- Level2.cc
+++ Level2.cc
@@ -1,16 +1,32 @@
 #include "Level2.h"
 
-Level2::Level2() {
+Level2::Level2() = default;
 
-};
-
-Level2::~Level2() {
-
-};
+Level2::~Level2() = default;
 
 
 shared_ptr<Block> Level2::makeBlock() {
+    shared_ptr<Block> block;
 
+    int num = rand() % 7;
+
+    if(!num) {
+        block = make_shared<Block>(makeSBlock());
+    } else if(num == 1) {
+        block = make_shared<Block>(makeZBlock());
+    } else if(num == 2) {
+        block = make_shared<Block>(makeIBlock());
+    } else if(num == 3) {
+        block = make_shared<Block>(makeJBlock());
+    } else if(num == 4) {
+        block = make_shared<Block>(makeLBlock());
+    } else if(num == 5) {
+        block = make_shared<Block>(makeOBlock());
+    } else if(num == 6) {
+        block = make_shared<Block>(makeTBlock());
+    }
+
+    return block;
 }
 
 
